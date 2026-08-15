e7e6e1195588a887a954a0804e3583c0e4724e79
miiidev <ahmad.syahmi3211@gmail.com>
2026-08-16 02:55:16 +0800

    feat: assemble new sections, reduced-motion config, remove particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
+import { MotionConfig } from 'framer-motion';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
 import SkillsSection from './components/SkillsSection';
+import TimelineSection from './components/TimelineSection';
 import ProjectsSection from './components/ProjectsSection';
 import ContactFooter from './components/ContactFooter';
 import SideStepper from './components/SideStepper';
 import BackToTop from './components/BackToTop';
-import Particles from './components/Particles';
 
 export default function App() {
   return (
     <ThemeProvider>
-    <div className="min-h-screen bg-canvas text-copy selection:bg-elevated">
-      <Particles />
-      <Navbar />
-      <SideStepper />
-      <BackToTop />
-      <HeroSection />
-      <main className="px-6 md:px-12 max-w-6xl mx-auto overflow-hidden">
-        <SkillsSection />
-        <ProjectsSection />
-        <ContactFooter />
-      </main>
-    </div>
+      <MotionConfig reducedMotion="user">
+        <div className="min-h-screen bg-canvas text-copy selection:bg-elevated">
+          <Navbar />
+          <SideStepper />
+          <BackToTop />
+          <HeroSection />
+          <main className="px-6 md:px-12 max-w-6xl mx-auto overflow-hidden">
+            <AboutSection />
+            <SkillsSection />
+            <TimelineSection />
+            <ProjectsSection />
+            <ContactFooter />
+          </main>
+        </div>
+      </MotionConfig>
     </ThemeProvider>
   );
 }
